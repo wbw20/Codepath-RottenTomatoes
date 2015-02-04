aa1b4b946439fb52d8fd062f213e0cebc820242a
William B. Wettersten <wwettersten@zendesk.com>
2015-02-04 15:40:14 -0800

    Requesting photos from the instagrams.

--- Drinkr/PhotosViewController.swift
+++ Drinkr/PhotosViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class PhotosViewController: UIViewController {
 
+    var photos: NSArray! = []
+
+    @IBOutlet var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var clientId = "ce1848d0d33e4833b0ca6beac05ac210"
+        
+        var url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
+        var request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
+            self.photos = responseDictionary["data"] as NSArray
+            self.tableView.reloadData()
+            
+            println("response: \(self.photos)")
+        }
         // Do any additional setup after loading the view.
     }
 
